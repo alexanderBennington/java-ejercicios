2790f05c8821bfad1eee730f01266b6d36689233
alexanderBennington <kevalexan15@outlook.com>
2025-02-13 14:39:38 -0600

    18

---
+++ 18-PromedioCalificaciones/PromedioCalificaciones.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PromedioCalificaciones {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numeroCalificaciones;
+        int i;
+        float[] calificaciones;
+        float promedio = 0;
+        char continuar;
+        boolean siContinuar = true;
+        do{
+            System.out.print("""
+                                *** Promedio de calificaciones ***
+                                Número de calificaciones: 
+                                """);
+            numeroCalificaciones = Integer.parseInt((scan.nextLine()));
+            calificaciones = new float[numeroCalificaciones]; 
+            for(i = 0; i < numeroCalificaciones; i++){
+                System.out.print("\nIngrese la califiacion numero " + i + ": ");
+                calificaciones[i] = Float.parseFloat(scan.nextLine());
+                promedio += calificaciones[i];
+            }
+            promedio /= numeroCalificaciones;
+            System.out.printf("\nEl promedio es de %.2f\n", promedio);
+            System.out.println("Quieres calcular otro promedio? s/n: ");
+            continuar = scan.next().charAt(0);
+            if(!String.valueOf(continuar).equalsIgnoreCase("s")){
+                siContinuar = false;
+                System.out.println("Hasta luego!");
+            }
+        } while(siContinuar);
+        scan.close();
+    }
+}
